63e7d84f42191c14a87afba9dab8794fd6111504
knya <johsvenn@student.chalmers.se>
2019-03-29 11:32:11 +0100

    change crypt.cpp so it uses RSA keypair instead of password

--- src/crypt.cpp
+++ src/crypt.cpp
@@ -16,68 +16,33 @@
  *      Check main functions for better usage
  * */
 
+using namespace Poco::Crypto;
+
 class Crypt{
-    Poco::Crypto::Cipher* cipher;
-    public: 
+
+private:
+    Cipher* cipher;
+    CipherFactory &factory = CipherFactory::defaultFactory();
+
+public:
     //Create an instance and a cipher in one
-    Crypt(std::string password){
-        std::cout << "Created a new instance of crypt using a password" << "\n";
-        this->createCipher(password);
-    }
-    Crypt(){
-        std::cout << "Created a new instance of the crypt class" << "\n";
-    }
-    
-    //Create a cipher that is used to decrypt and encrypt messages
-    Poco::Crypto::Cipher* createCipher(std::string password, std::string salt, std::string digest, std::string method){
-        Poco::Crypto::CipherFactory& factory = Poco::Crypto::CipherFactory::defaultFactory();
-        Poco::Crypto::CipherKey key(method, password, salt, ITERATIONS, digest);
+    Crypt() {
+        RSAKey key(RSAKey(RSAKey::KL_2048, RSAKey::EXP_LARGE));
         cipher = factory.createCipher(key);
-        return cipher;
     }
 
-    //Create a cipher that is used to decrypt and encrypt messages with default settings
-    Poco::Crypto::Cipher* createCipher(std::string password){
-        Poco::Crypto::CipherFactory& factory = Poco::Crypto::CipherFactory::defaultFactory();
-        std::string salt(SALT);
-        std::string digest (DIGEST);
-        Poco::Crypto::CipherKey key(METHOD, password, salt, ITERATIONS, digest);
+    Crypt(std::string publicKey, std::string privateKey) {
+        RSAKey key(RSAKey(publicKey, privateKey));
         cipher = factory.createCipher(key);
-        return cipher;
     }
 
-    //Encryptes a string
-    std::string encrypt(std::string a, Poco::Crypto::Cipher* cipher){
-        std::string encrypted = cipher->encryptString(a, Poco::Crypto::Cipher::ENC_BASE64);
-        return encrypted;
-    }
-    
     //Encryptes a string
     std::string encrypt(std::string a){
-        std::string encrypted = cipher->encryptString(a, Poco::Crypto::Cipher::ENC_BASE64);
-        return encrypted;
-    }
-
-    //Decrypt a string
-    std::string decrypt(std::string a, Poco::Crypto::Cipher* cipher){
-        std::string decrypt = cipher->decryptString(a, Poco::Crypto::Cipher::ENC_BASE64);
-        return decrypt;
+        return cipher->encryptString(a, Poco::Crypto::Cipher::ENC_BASE64);;
     }
     //Decrypt a string
     std::string decrypt(std::string a){
-        std::string decrypt = cipher->decryptString(a, Poco::Crypto::Cipher::ENC_BASE64);
-        return decrypt;
+        return cipher->decryptString(a, Poco::Crypto::Cipher::ENC_BASE64);;
     }
 
 };
-
-int main(){
-    std::string message = "My secret message";
-    Crypt* childCrypt = new Crypt("password");
-    std::string encryptedMessage = childCrypt->encrypt(message);
-    std::cout << encryptedMessage << "\n" ;
-    std::string decryptedMessage = childCrypt->decrypt(encryptedMessage);
-    std::cout << decryptedMessage << "\n" ;
-    return 0;
-}
-
