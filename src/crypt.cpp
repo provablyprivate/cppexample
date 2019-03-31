153c641242905e7eed8f4eab08e274f0cea49488
Ausut <robert.finne@gmail.com>
2019-03-31 22:57:56 +0200

    add signing and verification

--- src/crypt.cpp
+++ src/crypt.cpp
@@ -3,6 +3,8 @@
 #include "Poco/Crypto/CipherFactory.h"
 #include "Poco/Crypto/CipherKey.h"
 #include "Poco/Crypto/RSAKey.h"
+#include "Poco/Crypto/RSADigestEngine.h"
+#include "Poco/Crypto/DigestEngine.h"
 #include <iostream>
 #include <string.h>
 #include <stdexcept>
@@ -11,7 +13,7 @@
 /*  Usage
  *      Create an instance of the crypt class.
  *      Then use the methods given to encrypt
- *      and decrypt std::strings
+ *      and decrypt std::string
  *
  *      Check main functions for better usage
  * */
@@ -22,27 +24,56 @@ class Crypt{
 
 private:
     Cipher* cipher;
+    RSADigestEngine* digestEngine;
     CipherFactory &factory = CipherFactory::defaultFactory();
 
 public:
     //Create an instance and a cipher in one
     Crypt() {
         RSAKey key(RSAKey(RSAKey::KL_2048, RSAKey::EXP_LARGE));
+        digestEngine = new RSADigestEngine(key);
         cipher = factory.createCipher(key);
     }
 
     Crypt(std::string publicKey, std::string privateKey) {
         RSAKey key(RSAKey(publicKey, privateKey));
+        digestEngine = new RSADigestEngine(key);
         cipher = factory.createCipher(key);
     }
-
+    
+    // Creates an instance for when only the public key is known
+    Crypt(std::string publicKey) { 
+        RSAKey key(publicKey);
+        digestEngine = new RSADigestEngine(key);
+        cipher = factory.createCipher(key);
+        
+    }
+    
     //Encryptes a string
     std::string encrypt(std::string a){
-        return cipher->encryptString(a, Poco::Crypto::Cipher::ENC_BASE64);;
+        return cipher->encryptString(a, Poco::Crypto::Cipher::ENC_BASE64);
     }
     //Decrypt a string
     std::string decrypt(std::string a){
         return cipher->decryptString(a, Poco::Crypto::Cipher::ENC_BASE64);;
     }
-
+    
+    // Signs a string with private key
+    std::string sign(std::string a) {
+        digestEngine->reset();
+        digestEngine->update(a);
+        std::vector<unsigned char> v = digestEngine->signature();
+        return std::string(v.begin(), v.end());
+    }
+    
+    // Verify digest against data
+    bool verify(std::string data, std::string digest) {
+        digestEngine->reset();
+        digestEngine->update(data);
+        std::vector<unsigned char> v(digest.begin(), digest.end());
+        return digestEngine->verify(v);
+    }
+    
 };
+
+
--- test/json_test.cpp
+++ test/json_test.cpp
@@ -4,12 +4,18 @@
 #include "Poco/JSON/Parser.h"
 #include "Poco/Dynamic/Var.h"
 #include "Poco/JSON/Object.h"
+#include "Poco/Crypto/DigestEngine.h"
+#include "Poco/Crypto/RSAKey.h"
+#include "Poco/Crypto/RSADigestEngine.h"
 #include "../src/crypt.cpp"
+
+
+
 
 using namespace Poco;
 
 int main() {
-    Poco::FileInputStream fileStream("../test/pdata.json", std::ios::binary);
+    Poco::FileInputStream fileStream("./test/pdata.json", std::ios::binary);
 
     std::string json_data;
     StreamCopier::copyToString(fileStream, json_data);
@@ -22,10 +28,36 @@ int main() {
     std::cout << encryptedValue << std::endl;
 
     std::string message = "some private data";
-    auto * crypt = new Crypt("../test/rsa_key.pub","../test/rsa_key");
+    auto * cryptPubPr = new Crypt("./test/rsa_key.pub","./test/rsa_key");
 
 //    std::string encryptedMessage = crypt->encrypt(message);
 //    std::cout << encryptedMessage << "\n" ;
-    std::string decryptedMessage = crypt->decrypt(encryptedValue);
-    std::cout << decryptedMessage << "\n" ;
+    std::string decryptedMessage = cryptPubPr->decrypt(encryptedValue);
+    std::cout << decryptedMessage << "\n";
+    
+    
+    // Encrypt when only the public key is known:    
+    auto * cryptPub = new Crypt("./test/rsa_key.pub");
+    std::string encryptedMessage = cryptPub->encrypt(message);
+    
+    // Decrypt it with private key:
+    std::string decryptedMessage2 = cryptPubPr->decrypt(encryptedMessage);
+    std::cout << decryptedMessage2;
+    
+    
+    
+    // Sign (with private key):
+    std::string data = "some data to sign";
+    std::string dig = cryptPubPr->sign(data);
+    
+    // Verify digest against data (with only public key):
+    bool success = cryptPub->verify(data, dig);
+    if (success) std::cout << "OK";
+    
+    
+   
+    
+    
+    
+    
 }
