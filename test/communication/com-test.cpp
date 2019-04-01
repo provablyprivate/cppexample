c246e3b28b34824fcda04950d11349d0c0c10598
adihrustic <hrustic@student.chalmers.se>
2019-04-01 16:54:45 +0200

    removed trash
    Signed-off-by: adihrustic <hrustic@student.chalmers.se>

--- test/communication/com-test.cpp
+++ test/communication/com-test.cpp
@@ -12,56 +12,6 @@
 
 using namespace Poco;
 using namespace std;
-
-#include <algorithm>
-#include <stdexcept>
-
-std::string hex_to_string(const std::string& input)
-{
-    static const char* const lut = "0123456789ABCDEF";
-    size_t len = input.length();
-    if (len & 1) throw std::invalid_argument("odd length");
-
-    std::string output;
-    output.reserve(len / 2);
-    for (size_t i = 0; i < len; i += 2)
-    {
-        char a = input[i];
-        const char* p = std::lower_bound(lut, lut + 16, a);
-        if (*p != a) throw std::invalid_argument("not a hex digit");
-
-        char b = input[i + 1];
-        const char* q = std::lower_bound(lut, lut + 16, b);
-        if (*q != b) throw std::invalid_argument("not a hex digit");
-
-        output.push_back(((p - lut) << 4) | (q - lut));
-    }
-    return output;
-}
-
-
-
-// Creates generic JSON object
-Dynamic::Var createVarJSON(string stringJSON) {
-    JSON::Parser p;
-    return p.parse(stringJSON);
-}
-
-// Get data from json
-string getDataJSON(Dynamic::Var variable, string data) {
-    JSON::Object::Ptr obj = variable.extract<JSON::Object::Ptr>();
-    return obj->get(data).toString();
-}
-
-// Put data in json
-void putDataJSON(Dynamic::Var variable, string key, string value) {
-    JSON::Object::Ptr obj = variable.extract<JSON::Object::Ptr>();
-    obj->set(key, value);
-}
-
-// Prints file contents
-string read(string location) {
-    string tmp = "";
     ifstream file(location);
     StreamCopier::copyToString(file, tmp);
     file.close();
