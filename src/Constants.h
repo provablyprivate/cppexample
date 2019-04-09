74ec46f9b6919e9d13d1c9f7e69c8aacd96f7338
Ausut <robert.finne@gmail.com>
2019-04-09 10:52:50 +0200

    constants

---
+++ src/Constants.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+const std::string LOCALHOST = "127.0.0.1";
+
+const int PORT_BASE         = 30000;
+const int O_INTERNAL_PORT   = PORT_BASE + 1;
+const int O_EXTERNAL_PORT_1 = PORT_BASE + 2;
+const int O_EXTERNAL_PORT_2 = PORT_BASE + 3;
+const int I_INTERNAL_PORT   = PORT_BASE + 4;
+const int I_EXTERNAL_PORT_1 = PORT_BASE + 5;
+const int I_EXTERNAL_PORT_2 = PORT_BASE + 6;
+
+const std::string connectionTerminator = "bye";
